8748d743ba4be9af0935f75bae37ec6dd2386b28
U-CORP\tfuglsang <tfuglsang@CPHTFUGLESANG.corp.intra-gnn.com>
2016-10-04 10:36:06 +0200

    added models for box, line and diagram

---
+++ ClassDiagram/ClassDiagram/Model/IBox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDiagram.Model
+{
+    public interface IBox
+    {
+        string Label { get; set; }  // The name of the box shown in the View
+        double Height { get; set; } 
+        double Width { get; }
+        int Number { get; }         
+        double X { get; }           // Position in the canvas
+        double Y { get; }           // Position in the canvas
+        EBox Type { get; }        // Square, circle, etc.
+    }
+}

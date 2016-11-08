dff870b16ebf38f5ec70d0cb302e90af4a732a67
Witnessthis <emil.j.granberg@gmail.com>
2016-11-08 11:09:45 +0100

    Added missed files

---
+++ ClassDiagram/ClassDiagram/Model/Fields.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassDiagram.Model
+{
+    public class Fields
+    {
+        public Fields(String aField)
+        {
+            Field = aField;
+        }
+        public String Field { get; set; }
+    }
+}
---
+++ ClassDiagram/ClassDiagram/Model/Methods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassDiagram.Model
+{
+    public class Methods
+    {
+        public Methods(String aMethod)
+        {
+            Method = aMethod;
+        }
+        public String Method { get; set; }
+    }
+}

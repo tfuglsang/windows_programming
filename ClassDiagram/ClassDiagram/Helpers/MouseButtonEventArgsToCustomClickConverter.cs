b94e51eaa0e7093a401f323c7203cb9e211f4f4f
Quayed <matsondrup@gmail.com>
2016-11-08 09:45:21 +0100

    renaming

---
+++ ClassDiagram/ClassDiagram/Helpers/MouseButtonEventArgsToCustomClickConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+
+namespace ClassDiagram.Helpers
+{
+    internal class MouseButtonEventArgsToCustomClickConverter : IEventArgsConverter
+    {
+        public object Convert(object value, object parameter)
+        {
+            var args = (MouseButtonEventArgs) value;
+            var element = (FrameworkElement) parameter;
+
+            var point = args.GetPosition(element);
+            return new CustomClickArgs(point, args);
+        }
+    }
+}
